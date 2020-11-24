e9953bce2238a4987199596657917951817d72da
Xun Zhou <segentor@gmail.com>
2020-11-24 18:44:02 +0100

    updates

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,34 @@
 module.exports = {
-    title: "Nines",
-    description:
-        "Getting the things done in shortest time 🚀. A absolute must-have guide for lazy developer. ",
+    title: 'Nines',
+    description: 'Getting the things done in shortest time.',
     head: [
         [
-            "link",
+            'link',
             {
-                rel: "stylesheet",
-                href: `https://fonts.googleapis.com/css?family=Major+Mono+Display`
-            }
-        ]
+                rel: 'stylesheet',
+                href: `https://fonts.googleapis.com/css?family=Major+Mono+Display`,
+            },
+        ],
     ],
     plugins: [],
-    base: "/nines/",
+    base: '/nines/',
     themeConfig: {
-        lastUpdated: "Last Updated",
-        docsDir: "docs",
-        nav: [{ text: "HowTo", link: "/other/howTo" }],
+        lastUpdated: 'Last Updated',
+        docsDir: 'docs',
+        nav: [{ text: 'HowTo', link: '/other/howTo' }],
         sidebar: [
-            ["/", "🏠HOME"],
-            ["/bulma", " Bulma CSS"],
-            ["/react", " React"],
-            ["/vscode", " VS Code"],
-            ["/composer", " Composer"],
-            ["/docker", " Docker & Docker-Compose"],
-            ["/npm", " npm"],
-            ["/yarn", " Yarn"],
-            ["/github", " GitHub"],
-            ["/browser", " Browser"],
-            ["/symfony", " Symfony"]
-        ]
-    }
+            ['/', '🏠HOME'],
+            ['/bulma', ' Bulma CSS'],
+            ['/react', ' React'],
+            ['/vscode', ' VS Code'],
+            ['/composer', ' Composer'],
+            ['/docker', ' Docker & Docker-Compose'],
+            ['/npm', ' npm'],
+            ['/yarn', ' Yarn'],
+            ['/github', ' GitHub'],
+            ['/browser', ' Browser'],
+            ['/image', ' Image Processing'],
+            ['/symfony', ' Symfony'],
+        ],
+    },
 };
---
+++ docs/.vuepress/image.md
@@ -0,0 +1,10 @@
+# Image
+
+## resize image color
+```bash
+ brew install pngquant
+ 
+ pngquant 4 --skip-if-larger --strip --ext=.png --force layout.png
+ ## 4: convert image with only 4 colors and it will have the smallest size
+```
+> create cli clias, then can resize the png file with `png layout.png`
