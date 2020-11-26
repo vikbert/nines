f251934e498ac691846ca23487b28b809b9fbf49
Xun Zhou <segentor@gmail.com>
2020-11-26 08:13:54 +0100

    updates

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,35 @@
 module.exports = {
-    title: 'Nines',
-    description: 'Getting the things done in shortest time.',
-    head: [
-        [
-            'link',
-            {
-                rel: 'stylesheet',
-                href: `https://fonts.googleapis.com/css?family=Major+Mono+Display`,
-            },
-        ],
+  title: 'Nines',
+  description: 'Getting the things done in shortest time.',
+  head: [
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: `https://fonts.googleapis.com/css?family=Major+Mono+Display`,
+      },
     ],
-    plugins: [],
-    base: '/nines/',
-    themeConfig: {
-        lastUpdated: 'Last Updated',
-        docsDir: 'docs',
-        nav: [{ text: 'HowTo', link: '/other/howTo' }],
-        sidebar: [
-            ['/', '🏠HOME'],
-            ['/bulma', ' Bulma CSS'],
-            ['/react', ' React'],
-            ['/vscode', ' VS Code'],
-            ['/composer', ' Composer'],
-            ['/docker', ' Docker & Docker-Compose'],
-            ['/npm', ' npm'],
-            ['/yarn', ' Yarn'],
-            ['/github', ' GitHub'],
-            ['/browser', ' Browser'],
-            ['/image', ' Image Processing'],
-            ['/symfony', ' Symfony'],
-        ],
-    },
+  ],
+  plugins: [],
+  base: '/nines/',
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    docsDir: 'docs',
+    nav: [{ text: 'HowTo', link: '/other/howTo' }],
+    sidebar: [
+      ['/', '🏠HOME'],
+      ['/bulma', ' Bulma CSS'],
+      ['/react', ' React'],
+      ['/vscode', ' VS Code'],
+      ['/composer', ' Composer'],
+      ['/docker', ' Docker & Docker-Compose'],
+      ['/npm', ' npm'],
+      ['/yarn', ' Yarn'],
+      ['/github', ' GitHub'],
+      ['/browser', ' Browser'],
+      ['/image', ' Image Processing'],
+      ['/symfony', ' Symfony'],
+      ['/editorconfig', ' Editorconfig'],
+    ],
+  },
 };
--- docs/editorconfig.md
+++ docs/editorconfig.md
@@ -1,4 +1,15 @@
-# EditorConfig is awesome: https://github.io/vikbert/nines/editorconfig
+<div align="center">
+  <img src="https://brunolm.files.wordpress.com/2017/01/editorconfig-stickers.png" width="120">
+</div>
+
+
+# .editorconfig
+`EditorConfig` helps developers define and maintain `consistent coding styles` between different editors and IDEs. The EditorConfig project consists of a file format for defining coding styles and a collection of text editor plugins that enable editors to read the file format and adhere to the defined styles.
+
+
+## My favorite config
+```bash
+
 
 ##########################################
 # Common Settings
@@ -36,8 +47,16 @@ indent_size = 2
 trim_trailing_whitespace = false
 
 # Web Files
-[*.{htm,html,js,jsm,ts,tsx,css,sass,scss,less,svg,vue, svelte}]
+[*.{htm,html,css,sass,scss,less,svg}]
 indent_size = 2
+
+# javascripts Files
+[*.{js,jsm,ts,tsx,vue,svelte}]
+indent_size = 2
+
+# PHP Files
+[*.{php}]
+indent_size = 4
 
 # Batch Files
 [*.{cmd,bat}]
@@ -50,3 +69,6 @@ end_of_line = lf
 # Makefiles
 [Makefile]
 indent_style = tab
+
+
+```
